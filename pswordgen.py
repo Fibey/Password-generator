6decdc94f4578d793f971016888a43c716f8fe42
Fibey <141557612+Fibey@users.noreply.github.com>
2023-08-06 13:19:37 +0200

    Add files via upload

---
+++ pswordgen.py
@@ -0,0 +1,32 @@
+import random
+import time
+import cutie
+import os
+big_ass = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+littl_ass = big_ass.lower()
+bass = "0123456789"
+penis = """!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~"""
+rest = "ҾҿӀӁӂӃӄӅӆӇӈӉӊӋӌӍӎӏӐӑӒӓӔӕӖӗӘәӚӛӜӝӞӟӠӡӢӣӤӥӦӧӨөӪӫӬӭӮӯӰӱӲӳӴӵӶӷӸӹӺӻӼӽӾӿԀԁԂԃԄԅԆԇԈԉԊԋԌԍԎԏԐԑԒԓԔԕԖԗԘԙԚԛԜԝԞԟԠԡԢԣԤԥԦԧԨԩԪԫԬԭԮԯꙀꙁꙂꙃꙄꙅꙆꙇꙈꙉꙊꙋꙌꙍꙎꙏꙐꙑꙒꙓꙔꙕꙖꙗꙘꙙꙚꙛꙜꙝꙞꙟꙠꙡꙢꙣꙤꙥꙦꙧꙨꙩꙪꙫꙬꙭꙮ$꙯$꙰$꙱$꙲꙳$ꙴ$ꙵ$ꙶ$ꙷ$ꙸ$ꙹ$ꙺ$ꙻ$꙼$꙽꙾ꙿꚀꚁꚂꚃꚄꚅꚆꚇꚈꚉꚊꚋꚌꚍꚎꚏꚐꚑꚒꚓꚔꚕꚖꚗꚘꚙꚚꚛꚜꚝ$ꚞ$ͰͱͲͳʹ͵Ͷͷͺͻͼͽ;Ϳ΄΅Ά·ΈΉΊΌΎΏΐΑΒΓΔΕΖΗΘΙΚΛΜΝΞΟΠΡΣΤΥΦΧΨΩΪΫάέήίΰαβγδεζηθικλμνξοπρςστυφχψωϊϋόύώϏϐϑϒϓϔϕϖϗϘϙϚϛϜϝϞϟϠϡϢϣϤϥϦϧϨϩϪϫϬϭϮϯϰϱϲϳϴϵ϶ϷϸϹϺϻϼϽϾϿἀἁἂἃἄἅἆἇἈἉἊἋἌἍἎἏἐἑἒἓἔἕἘἙἚἛἜἝἠἡἢἣἤἥἦἧἨἩἪἫἬἭἮἯἰἱἲἳἴἵἶἷἸἹἺἻἼἽἾἿὀὁὂὃὄὅὈὉὊὋὌὍὐὑὒὓὔὕὖὗὙὛὝὟὠὡὢὣὤὥὦὧὨὩὪὫὬὭὮὯὰάὲέὴήὶίὸόὺύὼώᾀᾁᾂᾃᾄᾅᾆᾇᾈᾉᾊᾋᾌᾍᾎᾏᾐᾑᾒᾓᾔᾕᾖᾗᾘᾙᾚᾛᾜᾝᾞᾟᾠᾡᾢᾣᾤᾥᾦᾧᾨᾩᾪᾫᾬᾭᾮᾯᾰᾱᾲᾳᾴᾶᾷᾸᾹᾺΆᾼ᾽ι᾿῀῁ῂῃῄῆῇῈΈῊΉῌ῍῎῏ῐῑῒΐῖῗῘῙῚΊ῝῞῟ῠῡῢΰῤῥῦῧῨῩῪΎῬ῭΅`ῲῳῴῶῷῸΌῺΏῼ´῾"
+all = big_ass+littl_ass+bass+penis
+biggie = all+rest
+inches = int(input("Wie lang soll es lang sein:     "))
+f = open("passwort.txt", "w", encoding="utf-8")
+
+frage = cutie.prompt_yes_or_no("Use Unicode symbols?")
+yes_text = 'Yes'
+
+if frage:
+    for x in range(1):
+        password = "".join(random.choices(biggie,k=inches))
+        f.write(password)
+else:
+    for x in range(1):
+        password = "".join(random.choices(all,k=inches))
+        f.write(password)
+
+path = "passwort.txt"
+
+
+print("Passwort wurde in passwort.txt gespeichert")
+time.sleep(3)
+os.startfile(path)
